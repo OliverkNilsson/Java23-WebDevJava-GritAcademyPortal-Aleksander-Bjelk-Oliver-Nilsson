ca0906148522b1deef70b3af3b68101ccadedeb4
OliverkNilsson <oliverknilsson@gmail.com>
2024-02-16 11:13:03 +0100

    servlet fix lite JSP

--- slutprojekt_portal/src/main/java/servlets/LogInServlet.java
+++ slutprojekt_portal/src/main/java/servlets/LogInServlet.java
@@ -1,4 +1,17 @@
 package servlets;
 
-public class LogInServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet (name="/login", urlPatterns = "/login")
+public class LogInServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("JSPs/LogIn.jsp").forward(req, resp);
+    }
 }

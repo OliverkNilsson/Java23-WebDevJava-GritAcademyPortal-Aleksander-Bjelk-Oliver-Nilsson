9246a1a6d7dbfa9eb84795800191b502f725dfd6
OliverkNilsson <oliverknilsson@gmail.com>
2024-02-20 10:33:07 +0100

    Login fix + hämtar ID

--- slutprojekt_portal/src/main/java/servlets/LogOutServlet.java
+++ slutprojekt_portal/src/main/java/servlets/LogOutServlet.java
@@ -19,6 +19,5 @@ public class LogOutServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        return null;
     }
 }
